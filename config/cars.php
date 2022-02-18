f85793b43b6628113bcee007492d2c59b8ad44da
radfarelmira <radfar.elmira@gmail.com>
2022-02-18 13:57:50 +0100

    add cars.php file to config

---
+++ config/cars.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    [
+        "marca" => "Fiat",
+        "modello" => "500",
+        "cilindrata" => "1200",
+        "porte" => "3",
+        "img" => "https://wheels.iconmagazine.it/content/uploads/2020/11/Fiat-500-960x545.jpg",
+        
+    ],
+    [
+        "marca" => "Mercedes Benz",
+        "modello" => "S 600",
+        "cilindrata" => "6000",
+        "porte" => "4",
+        "img" => "https://img3.stcrm.it/images/1276502/1400x/4-00041180.jpg",
+        
+    ],
+    [
+        "marca" => "Citroen",
+        "modello" => "2 CV",
+        "cilindrata" => "600",
+        "porte" => "4",
+        "img" => "https://cdn.motor1.com/images/mgl/A9OP9/s1/citroen-2cv-charleston-1990.jpg",
+        
+    ],
+    [
+        "marca" => "Honda",
+        "modello" => "Civic",
+        "cilindrata" => "1600",
+        "porte" => "3",
+        "img" => "https://cdn.motor1.com/images/mgl/nn82G/s3/honda-civic-eg6-vti.jpg",
+        
+    ],
+    [
+        "marca" => "Lotus",
+        "modello" => "Elise",
+        "cilindrata" => "1800",
+        "porte" => "2",
+        "img" => "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lotus_Elise_Series_1.jpg/1920px-Lotus_Elise_Series_1.jpg",
+        
+    ],
+];
